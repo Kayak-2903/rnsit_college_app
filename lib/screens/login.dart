2c195a17b293f0a4d6fb506c9c012210cf059bf4
Kayak V Gornale <kayak.gornale@gmail.com>
2021-11-09 18:45:42 +0530

    modified logo

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -23,8 +23,14 @@ class _LoginState extends State<Login> {
                 margin: const EdgeInsets.only(bottom: 30, top: 30),
                 child: Image.asset(
                   kLogosCollegeLogo,
-                  width: MediaQuery.of(context).size.width * .50,
-                  height: MediaQuery.of(context).size.width * .50,
+                  width: MediaQuery.of(context).size.width <
+                          MediaQuery.of(context).size.height
+                      ? MediaQuery.of(context).size.width * .50
+                      : MediaQuery.of(context).size.height * .50,
+                  height: MediaQuery.of(context).size.width <
+                          MediaQuery.of(context).size.height
+                      ? MediaQuery.of(context).size.width * .50
+                      : MediaQuery.of(context).size.height * .50,
                   alignment: Alignment.center,
                   repeat: ImageRepeat.noRepeat,
                 ),
